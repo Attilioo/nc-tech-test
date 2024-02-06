3695fbef32d71f3526260c0b2e5326b498345c7d
Attilio Andrea Marotta <attilio@mekmo.studio>
2024-02-06 12:07:52 +0000

    removed cors and changed Port due to issue with '7000'

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import * as express from "express";
 
-export const app = express()
+export const app = express();
 
-app.set('json spaces', 2);
+app.set("json spaces", 2);
 
-app.get('/cards', async () => {
+app.get("/cards", async () => {
   // respond with a list of cards
-})
+  console.log("success");
+});
 
-app.get('/cards/:cardId/:sizeId?', () => {
+app.get("/cards/:cardId/:sizeId?", () => {
   // respond with card by id
-})
+});
